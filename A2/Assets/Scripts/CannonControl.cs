84bbc81a967fa5ee300a9339770ae48474779527
Matthew Adler <madler72@gmail.com>
2014-02-25 21:11:58 -0500

    Canon Rotation. Shooting next, then physics, then wind, then collision, then deformation.

--- A2/Assets/Scripts/CannonControl.cs
+++ A2/Assets/Scripts/CannonControl.cs
@@ -12,6 +12,9 @@ public class CannonControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKey("left"))
+			transform.Rotate(Vector3.forward * Time.deltaTime * 500);
+		if(Input.GetKey("right"))
+			transform.Rotate(Vector3.back * Time.deltaTime * 500);
 	}
 }
